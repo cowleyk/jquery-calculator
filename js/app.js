b6eea5ecf4d82df81427e01d501f32ce90cbf9c1
Kevin Cowley <cowley.kevin.l@gmail.com>
2016-10-20 14:56:29 -0600

    negative #s working,added comments to code

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-// console.log($('span.operator').text());
 
 var holder = '';
 var rightHolder = '';
+var oper = '';
 var opNum = false;
 var answer = 0;
 $('span').not('.operator').on('click', function(){
+  //select number buttons
   if(!opNum){
     num = ($(this).text());
     holder = holder + num;
     console.log(holder);
     $('#screen').text(holder);
   }
+  //boolean opNum dictates whether to concat integers to number on right or left side of operator
   if(opNum){
     rightNum = ($(this).text());
     rightHolder = rightHolder + rightNum;
@@ -21,29 +23,50 @@ $('span').not('.operator').on('click', function(){
   }
 })
 
+
+$('.operator').not('#equals').not('#clear').on('click', function(){
+  //select math operator button
+  if(holder===''){
+    holder = '-';
+    $('#screen').text(holder);
+  }
+  //if no number is input, '-' means neg.# on left side of operator
+  else if(oper && ($(this).text())==='-'){
+    rightHolder = '-';
+    $('#screen').text(holder+' '+oper+' '+rightHolder)
+  }
+  //if an operator already has been chosen, '-' is for neg.# on right side
+  else{
+    oper = ($(this).text());
+    opNum = true;
+    $('#screen').text(holder+' '+oper);
+  }
+  //if a number has already been entered and no other operator selected it must be a minus sign or just the other operator.  opNum boolean switches to true, next input starts right side number
+
+})
+
 $('#clear').on('click', function(){
   holder = '';
   rightHolder = '';
+  oper = '';
+  opNum = false;
+  answer = 0;
   $('#screen').text(holder);
-})
-
-$('.operator').not('#equals').not('#clear').on('click', function(){
-  oper = ($(this).text());
-  opNum = true;
-  $('#screen').text(holder+' '+oper);
-
+  //resets all variables
 })
 
 $('#equals').on('click', function(){
-  leftInteger = parseInt(holder);
-  rightInteger = parseInt(rightHolder);
-  // operInt = parseInt(oper);
+  leftInteger = (parseInt(holder));
+  rightInteger = (parseInt(rightHolder));
+  //turn left&right numbers into into integers
   opNum = false
   holder = '';
   rightHolder = '';
+  //resets variable and goes back to left side
   console.log('left = ' + leftInteger);
   console.log('right = ' + rightInteger);
   console.log(oper);
+  //switch stmt converts button pushed into mathmatical fxn
   switch (true) {
     case oper==='÷':
       answer = leftInteger/rightInteger;
@@ -57,21 +80,23 @@ $('#equals').on('click', function(){
     case oper ==='+':
       answer = leftInteger+rightInteger;
       break;
+    case oper ==='':
+      answer = leftInteger;
+      break;
     default:
       answer = 'Error';
   }
+
   console.log('answer = ' + answer);
   if(answer%1===0){
-    $('#screen').text(answer);
+    //if answer is a whole number, show no decimal places
+    $('#screen').text(parseFloat(answer));
   }
   else{
-  $('#screen').text(answer.toFixed(4));
+    //if answer is decimal, show only up to 4 decimal places
+    $('#screen').text(parseFloat(answer.toFixed(4)));
   }
-  //use switch statement for each operator carouse
-    //eg; switch oper === '+'
-      // leftInteger + rightInteger
-      //case oper === 'x'
-      // leftInteger * rightInteger
+
 
 
 })
